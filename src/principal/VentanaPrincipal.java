0138139d9a30e8b180286059cf1ed7fa7510dfa6
Emanuel Barreto <enigbaires@gmail.com>
2022-05-11 00:17:27 -0300

    Menu con funciones

--- src/principal/VentanaPrincipal.java
+++ src/principal/VentanaPrincipal.java
@@ -7,13 +7,20 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+import dominio.PanelListadoPeliculas;
+
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
 public class VentanaPrincipal extends JFrame {
 
 	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
+	private JMenuBar menuBar;
 	
 
 	/**
@@ -24,8 +31,46 @@ public class VentanaPrincipal extends JFrame {
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		menuBar = new JMenuBar();
+		menuBar.setToolTipText("");
+		menuBar.setBounds(45, 0, 97, 21);
+		contentPane.add(menuBar);
+		
+		JMenu mnPeliculas = new JMenu("Peliculas");
+		mnPeliculas.setBounds(10, 2, 55, 19);
+		contentPane.add(mnPeliculas);
+		
+		JMenuItem mntmAgregar = new JMenuItem("Agregar");
+		mntmAgregar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+//				contentPane.removeAll();
+//				PanelAgregarPeliculas agregar = new PanelAgregarPeliculas();
+//				agregar.setDefaultListModel(listModel);
+//				contentPane.add(agregar);
+//				contentPane.repaint();
+//				contentPane.revalidate();
+				
+			}
+		});
+		mnPeliculas.add(mntmAgregar);
+		
+		JMenuItem mntmListar = new JMenuItem("Listar");
+		mntmListar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+//				contentPane.removeAll();
+//				PanelListadoPeliculas listado = new PanelListadoPeliculas();
+//				listado.setDefaultListModel(listModel);
+//				contentPane.add(listado);
+//				contentPane.repaint();
+//				contentPane.revalidate();
+				
+			}
+		});
+		mnPeliculas.add(mntmListar);
 	}
-
 }
